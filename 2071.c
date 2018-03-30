2896eb8fa554b33f06402b68838fc5589cc9e6b5
eJlp18 <517602852@qq.com>
2018-03-30 12:43:59 +0800

    水题哈哈

---
+++ 2071.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+double find_max(double s[] , int n)
+{
+	double max ;
+	max = s[0];
+	for(int i = 1; i < n; i++)
+	{
+		if(s[i] > max)
+		{
+			max = s[i];
+		}
+	}
+	return max;
+}
+int main()
+{
+	int n;
+	while(~scanf("%d",&n))
+	{
+		while(n--)
+		{
+            int m;
+            scanf("%d",&m);
+		    double c[m];
+		    for (int j = 0; j < m ;j++)
+		    {
+		        scanf("%lf",&c[j]);
+		    }
+		    printf("%.2wlf\n",find_max(c , m));
+		}	
+	}
+	return 0;
+}
